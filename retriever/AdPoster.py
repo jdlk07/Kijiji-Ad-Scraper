3e92e85f603ba9027e02eaa676b34e141e725889
Janath Dinesh Perera <jdlk07@gmail.com>
2019-04-25 18:55:13 +0530

    Changed scrapy crawler to be called from custom python script AdPoster.py
    AdPoster.py now prompts the user for the url to crawl when the script is initiated.

--- retriever/AdPoster.py
+++ retriever/AdPoster.py
@@ -4,37 +4,40 @@ from scrapy.item import Item
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from scrapy.crawler import CrawlerProcess
-
+from scrapy.utils.project import get_project_settings
 
 
 class ImgData(Item):
+#Required to retrieve images via scrapy.
     image_urls=scrapy.Field()
     images=scrapy.Field()
     image_name=scrapy.Field()
 
 class DataRetriever(scrapy.Spider):
+#The scrapy spider. Crawls URL or list of URL's for the following information: Title, Description, Address, Price, Images. Save this information in an "ads" folder under the ad reference number.
     name = "data"
 
-    def __init__(self):
-        chrome_options = Options()
+    # def __init__(self):
+        # chrome_options = Options()
         #chrome_options.add_argument("--disable-extensions")
         # chrome_options.add_argument("--disable-gpu")
         # chrome_options.add_argument("--headless")
         # self.driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
     def start_requests(self):
-        urls = [
-            'https://www.kijiji.ca/v-bed-mattress/mississauga-peel-region/brand-new-queen-size-pillow-top-mat-box-both-for-249/1324601512',
-            'https://www.kijiji.ca/v-bed-mattress/city-of-toronto/wholesale-furniture-warehouse-we-beat-any-price-lowest-price-guaranteed-www-aerys-ca/c3770284',
-            'https://www.kijiji.ca/v-camera-camcorder-lens/city-of-toronto/dji-ronin-m-w-nanuk-940-hardcase/1429494111'
-        ]
+        # urls = [
+        #     'https://www.kijiji.ca/v-bed-mattress/mississauga-peel-region/brand-new-queen-size-pillow-top-mat-box-both-for-249/1324601512',
+        #     'https://www.kijiji.ca/v-bed-mattress/city-of-toronto/wholesale-furniture-warehouse-we-beat-any-price-lowest-price-guaranteed-www-aerys-ca/c3770284',
+        #     'https://www.kijiji.ca/v-camera-camcorder-lens/city-of-toronto/dji-ronin-m-w-nanuk-940-hardcase/1429494111'
+        # ]
+        urls = [URL]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-    
+
     def parse(self, response):
         # self.driver.get(response.url)
         # self.driver.close()
-        ref_num = response.url.split("/")[-1]
+        ref_num = response.url.split("/")[-1].split("?")[0]
         title = response.xpath('//h1[@itemprop="name"]/text()').get()
         description = response.xpath('//div[@itemprop="description"]/p/text()').getall()
         address = response.xpath('//span[@itemprop="address"]/text()').get()
@@ -54,15 +57,16 @@ class DataRetriever(scrapy.Spider):
             for paragraph in description:
                 descriptionFile.write(paragraph.encode('utf-8') + "\n")
         with open(address_filename, 'wb') as addressFile:
-            addressFile.write(address.encode('utf-8'))        
+            addressFile.write(address.encode('utf-8'))
         with open(price_filename, 'wb') as priceFile:
-            priceFile.write(price.encode('utf-8'))                   
+            priceFile.write(price.encode('utf-8'))
         yield ImgData(image_urls=image_urls, image_name="%s/images/image" %ref_num)
 
-if __name__ == "__main__":
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-
-    process.crawl(MySpider)
+def GetData(URL):
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(DataRetriever, URL=URL)
     process.start() # the script will block here until the crawling is finished
+
+if __name__ == "__main__":
+    URL = raw_input("Enter URL\n")
+    GetData(URL)
--- retriever/retriever/pipelines.py
+++ retriever/retriever/pipelines.py
@@ -16,14 +16,13 @@ class CustomImageNamePipeline(ImagesPipeline):
     #             for x in item.get('image_urls', [])]
 
     def get_media_requests(self, item, info):
-        for i, image_url in enumerate(item.get('image_urls', [])):
+        for i, image_url in enumerate(item.get('image_urls', []), 1):
             yield Request(image_url, meta={'image_name': item['image_name'] + str(i)})
 
     def file_path(self, request, response=None, info=None):
         return '%s.jpg' % request.meta['image_name']
 
-        
+
 class RetrieverPipeline(object):
     def process_item(self, item, spider):
         return item
-
